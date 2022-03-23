5cc0ecabd3dfe760746a0179aa56acb419f48362
JosipSkrlec <jskrlec1997@gmail.com>
2022-03-23 18:37:09 +0100

    Update enemy patrolling, default state!

--- Assets/!Assets/Scripts/Enemy.cs
+++ Assets/!Assets/Scripts/Enemy.cs
@@ -4,29 +4,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float _horizontalSpeed;
+    [SerializeField] private bool _isPatrolling = true;
+    [SerializeField] private float _walkSpeed;
     [Space(5)]
     [SerializeField] private Transform _attackPoint;
     [SerializeField] private Transform _wallPatrolPoint;
     [SerializeField] private Transform _groundPatrolPoint;
+    [SerializeField] private LayerMask _playerLayer;
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private LayerMask _wallLayer;
+    [Space(5)]
+    [SerializeField] private Transform _agroDistanceTransform;
 
     private Rigidbody2D _thisRB;
     private Animator _thisAnim;
-    float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
         _thisAnim = GetComponent<Animator>();
         _thisRB = GetComponent<Rigidbody2D>();
+
+        StartPatrolling();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_isPatrolling == false)
+        {
+            return;
+        }
+
         bool groundCheck = Physics2D.OverlapCircle(_groundPatrolPoint.position,0.25f,_groundLayer);
         bool wallCheck = Physics2D.OverlapCircle(_wallPatrolPoint.position,0.25f,_wallLayer);
 
@@ -35,11 +44,23 @@ public class Enemy : MonoBehaviour
         {
             HorizontalFlip();
         }
+
+        var test = Physics2D.Raycast(transform.position, transform.right, _playerLayer);
+        Debug.Log(test);
+        if (test == true)
+        {
+            Debug.Log("FOUND " + test.transform.name);
+        }
     }
 
     private void FixedUpdate()
     {
-       _thisRB.velocity = new Vector2(transform.right.x * _horizontalSpeed, _thisRB.velocity.y);
+        if (_isPatrolling == false)
+        {
+            return;
+        }
+
+        _thisRB.velocity = new Vector2(transform.right.x * _walkSpeed, _thisRB.velocity.y);
     }
 
     private void HorizontalFlip()
@@ -47,14 +68,31 @@ public class Enemy : MonoBehaviour
         transform.Rotate(0, 180, 0);
     }
 
+    private void StopPatroling()
+    {
+        _thisAnim.SetTrigger("Idle");
+
+        _isPatrolling = false;
+    }
+
+    private void StartPatrolling()
+    {
+        _thisAnim.SetTrigger("Walk");
+
+        _isPatrolling = true;
+    }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawSphere(_groundPatrolPoint.position, 0.25f);
         Gizmos.DrawSphere(_wallPatrolPoint.position, 0.25f);
-        Gizmos.DrawSphere(_attackPoint.position, 0.25f);
-    }
 
+        Gizmos.DrawSphere(_attackPoint.position, 0.25f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position+ new Vector3(0.0f, 1.0f, 0.0f), _agroDistanceTransform.position);
+
+    }
 
 }
