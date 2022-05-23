d9ea13d4cd5af50bfecebc4531b4f7d7b64861cc
JSkrlec <jskrlecdev@gmail.com>
2022-05-23 19:09:26 +0100

    Update UI

--- Assets/!Assets/Scripts/UIMenuController.cs
+++ Assets/!Assets/Scripts/UIMenuController.cs
@@ -4,37 +4,55 @@ using UnityEngine.SceneManagement;
 
 public class UIMenuController : MonoBehaviour
 {
-    [SerializeField] private CanvasGroup _backgroundFadedImage;
-    [SerializeField] private CanvasGroup _settingsPanel;
+    [SerializeField] private float _screenAnimationSpeed = 0.25f;
+    [Space(5)]
+    [SerializeField] private CanvasGroup _mainMenu;
+    [SerializeField] private CanvasGroup _settingsMenu;
+    [Space(3)]
+    [SerializeField] private CanvasGroup _controlsSettings;
+    [SerializeField] private CanvasGroup _audioSettings;
+    [SerializeField] private CanvasGroup _videoSettings;
 
 
-    private void Awake()
+
+    private void Start()
     {
-        _backgroundFadedImage.alpha = 0.0f;
-        _backgroundFadedImage.blocksRaycasts = false;
+        HideImediateCG(_settingsMenu);
+        HideImediateCG(_controlsSettings);
+        HideImediateCG(_audioSettings);
+        HideImediateCG(_videoSettings);
 
-        _settingsPanel.alpha = 0.0f;
-        _settingsPanel.blocksRaycasts = false;
+    }
+
+    public void HideImediateCG(CanvasGroup cg)
+    {
+        cg.blocksRaycasts = false;
+        cg.alpha = 0.0f;
     }
 
     public void StartTheGame()
     {
         SceneManager.LoadScene(1);
-
     }
-
     public void OpenSettings()
     {
-        _backgroundFadedImage.DOFade(1.0f,0.25f).OnComplete(() => _backgroundFadedImage.blocksRaycasts = true);
-        _settingsPanel.DOFade(1.0f,0.25f).OnComplete(() => _settingsPanel.blocksRaycasts = true);
+        _mainMenu.blocksRaycasts = false;
+        _settingsMenu.DOFade(1.0f, _screenAnimationSpeed).OnComplete(() =>
+                 _settingsMenu.blocksRaycasts = true
+        );
 
     }
-
     public void CloseSettings()
     {
-        _backgroundFadedImage.DOFade(0.0f, 0.25f).OnComplete(() => _backgroundFadedImage.blocksRaycasts = false);
-        _settingsPanel.DOFade(0.0f, 0.25f).OnComplete(()=> _settingsPanel.blocksRaycasts = false);
+        _settingsMenu.blocksRaycasts = false;
+        _settingsMenu.DOFade(0.0f, _screenAnimationSpeed).OnComplete(() =>
+                 _mainMenu.blocksRaycasts = false
+        );
 
+    }
+    public void ExitFromGame()
+    {
+        Application.Quit();
     }
 
 
